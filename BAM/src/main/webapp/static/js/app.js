fed2ef06be2630a1e6d423c2abde4779dde09c44
Matthew Young <rainick987@gmail.com>
2017-07-21 14:19:52 -0400

    register page

--- BAM/src/main/webapp/static/js/app.js
+++ BAM/src/main/webapp/static/js/app.js
@@ -1,7 +1,9 @@
 var app = angular.module('bam', ['ngRoute']);
 app.config(function($routeProvider, $locationProvider){
-	
+	$locationProvider.html5Mode(false).hashPrefix('');
 	$routeProvider.when("/",{
 		templateUrl: "static/pages/login.html"
+	}).when("/register",{
+		templateUrl: "static/pages/register.html"
 	}).otherwise({redirectTo: '/'})
 });
