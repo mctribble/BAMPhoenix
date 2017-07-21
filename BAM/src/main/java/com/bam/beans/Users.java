fb3dd3ba22a62c393bca3a7c3af1f560ddf0ff0f
johncollin1 <innoven_john@yahoo.com>
2017-07-21 15:00:59 -0400

    Finxed User Bean

--- BAM/src/main/java/com/bam/beans/Users.java
+++ BAM/src/main/java/com/bam/beans/Users.java
@@ -11,7 +11,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
