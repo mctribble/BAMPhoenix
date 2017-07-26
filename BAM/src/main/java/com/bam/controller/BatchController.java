fb257ee3fe46652d82db18aa82da37aae1fe5cea
Valentin Firpo <valfirpo90@live.com>
2017-07-26 10:13:06 -0400

    batches controler2

--- BAM/src/main/java/com/bam/controller/BatchController.java
+++ BAM/src/main/java/com/bam/controller/BatchController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,8 @@ public class BatchController
 	@Autowired
 	BatchService batchService;
 	
+	//@RequestMapping(value = "All", method = RequestMethod.GET, headers = "Accept=application/json")
+	@RequestMapping(value = "All", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public List<Batch> getBatchAll()
 	{
