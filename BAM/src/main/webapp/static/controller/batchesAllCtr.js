08b8f157609c9c2ca4e370a3bd3cf049f17e7131
Valentin Firpo <valfirpo90@live.com>
2017-07-27 11:58:56 -0400

    controller consoler.log removed

--- BAM/src/main/webapp/static/controller/batchesAllCtr.js
+++ BAM/src/main/webapp/static/controller/batchesAllCtr.js
@@ -1,7 +1,5 @@
 app.controller('batchesAllCtr', function($scope, $location, $http)
-{
-	console.log("in all controller");
-	
+{	
 	$scope.batches;
 	
 	$scope.getBatchesAll = function(){
@@ -11,10 +9,8 @@ app.controller('batchesAllCtr', function($scope, $location, $http)
 			method: 'GET'
 		})
 		.then(function success(response){
-			console.log("in success all controller");
 			$location.path('/batchesAll');
 		}, function error(response){
-			console.log("in err all controller");
 			$location.path('/batchesAll');
 		});
 	}
--- BAM/src/main/webapp/static/controller/loginController.js
+++ BAM/src/main/webapp/static/controller/loginController.js
@@ -7,9 +7,6 @@ app.controller('loginCtl', function($rootScope, $scope, $location, $http) {
 			email : $scope.email,
 			password : $scope.password
 		};
-
-		console.log(user.email + " " + user.password);
-		console.log();
 		
 		$http({
 			url: 'authenticate',
@@ -23,13 +20,13 @@ app.controller('loginCtl', function($rootScope, $scope, $location, $http) {
 		.then(function success(response){
 			$location.path('/home');
 			$scope.message = true;
-			$scope.msg = 'you logged in';
+			$scope.msg = 'Login successful';
 			$rootScope.user = response.data;
 			console.log(response.data);
 		}, function error(response){
 			$location.path('/');
 			$scope.message = true;
-			$scope.msg = 'you did not login';
+			$scope.msg = 'Login failed';
 		});
 	}
 	
