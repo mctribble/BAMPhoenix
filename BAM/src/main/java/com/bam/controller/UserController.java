b19f1501cdb629bdb73aaad43db62e9d61751e4a
Jonathan Joseph <lefort@mail.usf.edu>
2017-07-28 13:38:22 -0400

    Added Update Method

--- BAM/src/main/java/com/bam/controller/UserController.java
+++ BAM/src/main/java/com/bam/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.bam.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.bam.beans.Users;
 import com.bam.service.UsersService;
+
 
 @RestController
 @RequestMapping(value="/Users/")
@@ -33,5 +40,25 @@ public class UserController {
 		
 		//Retrieve and return users in a batch from the database
 		return userService.findUsersInBatch(batchId);
+	}
+	
+	@RequestMapping(value="Update.do", method=RequestMethod.POST, produces="application/json")
+	@ResponseBody
+	public void updateUser(@RequestBody String jsonObject, HttpSession session) {
+		Users currentUser = null;
+		System.out.println("jsonObject: " + jsonObject);
+		try {
+			currentUser = new ObjectMapper().readValue(jsonObject, Users.class);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		userService.addUser(currentUser);
+		
+		//Retrieve and return users in a batch from the database
 	}
 }
