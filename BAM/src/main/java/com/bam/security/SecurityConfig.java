2612d7a407ed25b4fb1c811b21082b5afad50ad1
David Foens <Foensd@yahoo.com>
2017-07-29 12:10:11 -0400

    Getting Servlet error when on EC2, added @Configuration to SecurityConfig to try to fix

--- BAM/src/main/java/com/bam/security/SecurityConfig.java
+++ BAM/src/main/java/com/bam/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.bam.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.web.authentication.logout.HttpStatusReturnin
 /*
  * 
  */
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
