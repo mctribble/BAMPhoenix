45aa078ddc1c50bb730f85ae5e055086c0933dcd
Matthew Young <rainick987@gmail.com>
2017-08-01 13:58:00 -0400

    An associate with no batch can not access the calendar page, and will be automatically redirected to the no batch view

--- BAM/src/main/webapp/static/controller/loginController.js
+++ BAM/src/main/webapp/static/controller/loginController.js
@@ -31,14 +31,16 @@ app.controller('loginCtl', function($rootScope, $scope, $location, $http) {
 				}, function error(progResponse){
 					$scope.msg = 'Batch Acquisition failed';
 				});
-			} else {
-				if(!$rootScope.user.batch && $rootScope.user.role == 1)
+			} else if($rootScope.user.role == 1) {
+				if(!$rootScope.user.batch)
 				{
 					$location.path('/noBatch');
 				}else{
 					$location.path('/home');
 				}
 				
+			}else{
+				$location.path('/batchesAll');
 			}
 			
 		}, function error(response){
