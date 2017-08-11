8137cb013b7e0579bcc7c9c849e2e4e5fc83b6d8
avantmathur <avantmathur2@gmail.com>
2017-08-11 13:24:06 -0400

    got rid of unused imports

--- BAM/src/main/java/com/bam/main/Driver.java
+++ BAM/src/main/java/com/bam/main/Driver.java
@@ -1,13 +1,5 @@
 package com.bam.main;
 
-import java.util.List;
-
-import org.apache.log4j.Logger;
-
-import com.bam.beans.Batch;
-import com.bam.service.BatchService;
-import com.bam.service.PasswordGenerator;
-
 
 public class Driver {
 
