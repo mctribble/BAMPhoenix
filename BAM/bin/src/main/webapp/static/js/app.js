98bc89e0ec46029133e09f63fb0073e87657ed48
Troy_K <textak64@gmail.com>
2017-08-11 13:49:34 -0400

    fixing app.js

--- BAM/bin/src/main/webapp/static/js/app.js
+++ BAM/bin/src/main/webapp/static/js/app.js
@@ -3,34 +3,34 @@ app.config(function($routeProvider, $locationProvider){
 	$locationProvider.html5Mode(false).hashPrefix('');
 	$routeProvider.when("/",{
 		templateUrl: "static/pages/login.html",
-		controller: 'loginController'
+		controller: 'loginCtl'
 	}).when("/batchesAll",{
 		templateUrl:"static/pages/batchesAll.html",
-		controller: "batchesAllController"
+		controller: "batchesAllCtr"
 	}).when("/register",{
 		templateUrl: "static/pages/register.html",
-		controller: "bamUserRegisterController"
+		controller: "registerCtrl"
 	}).when("/home",{
 		templateUrl: "static/pages/calendar.html",
-		controller: "calendarController"
+		controller: "uiCalendarCtrl"
 	}).when("/associates",{
 		templateUrl: "static/pages/ViewAssociates.html",
-		controller: "associateViewController"
+		controller: "associatesController"
 	}).when("/update",{
 		templateUrl: "static/pages/update.html",
-		controller: "associateUpdateController"
+		controller: "updateAssociateCtrl"
 	}).when("/reset",{
 		templateUrl: "static/pages/reset.html",
-		controller: "passwordResetController"
+		controller: "resetCtrl"
 	}).when("/batchesPast",{
 		templateUrl: "static/pages/batchesPast.html",
-		controller: "batchesPastController"
+		controller: "batchesPast"
 	}).when("/batchesFuture",{
 		templateUrl: "static/pages/batchesFuture.html",
-		controller: "batchesFutureController"
+		controller: "batchesFuture"
 	}).when("/editBatch", {
 		templateUrl: "static/pages/EditBatch.html",
-		controller: "batchEditController"
+		controller: "editBatchController"
 	}).when("/noBatch",{
 		templateUrl: "static/pages/NoBatch.html"
 	}).otherwise({redirectTo: '/'});
