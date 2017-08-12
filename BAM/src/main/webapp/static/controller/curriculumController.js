1f67edf9eda4018d409303890185358751fd6c36
Brian McKalip <brmckalip@gmail.com>
2017-08-12 13:20:26 -0400

    added some frontend functionality - mostly broken, pushing so I can work on my other computer

--- BAM/src/main/webapp/static/controller/curriculumController.js
+++ BAM/src/main/webapp/static/controller/curriculumController.js
@@ -1,6 +1,23 @@
 /**
  * Defines a controller to handle DOM manipulation of the Curriculum HTML page
  */
-app.controller("curriculumController", function($rootScope, $scope, $location, $http){
-	console.log("In Curr controller");
-});
+app.controller("curriculumController", 
+	function($rootScope, $scope, $location, $http){
+		$scope.topicFilter = "";
+		
+		console.log("In Curr controllers");
+		$scope.topics = [];
+		
+		$scope.topics.push({'name':"Java"});
+		$scope.topics.push({'name':"SQL"});
+		$scope.topics.push({'name':".NET"});
+		
+		console.log($scope.topics);
+		
+		$scope.subtopics = [];
+		
+		$scope.subtopics.push({'id':1, 'name':'core'});
+		$scope.subtopics.push({'id':1, 'name':'spring'});
+		$scope.subtopics.push({'id':1, 'name':'hibernate'});
+	}
+);
