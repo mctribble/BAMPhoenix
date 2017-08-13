abcc4b7cb0de4a4509b92a21be8beaaefe97ca3d
duncanhayward <duncanhayward7@gmail.com>
2017-08-13 17:27:06 -0400

    getting rid of replicated code

--- BAM/src/main/java/com/bam/controller/UserController.java
+++ BAM/src/main/java/com/bam/controller/UserController.java
@@ -11,11 +11,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bam.bean.Batch;
 import com.bam.bean.BamUser;
+import com.bam.bean.Batch;
 import com.bam.service.BatchService;
-
-import com.bam.service.BamUserService;
+import com.bam.service.UsersDetailsService;
 
 
 @RestController
@@ -26,73 +25,71 @@ public class UserController {
 	private final String batchID = "batchId";
 	
 	@Autowired
-
-	BamUserService bamUserService;
-
+	UsersDetailsService userService;
 
 	@Autowired
 	BatchService batchService;
 
 	@RequestMapping(value = "All", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	public List<BamUser> getAllUsers(){
-		return bamUserService.findAllUsers();
+	public List<BamUser> getAllUsers() {
+		return userService.findAllUsers();
 	}
 
 	@RequestMapping(value = "AllTrainers", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	public List<BamUser> getAllTrainers(){
-		return bamUserService.findByRole(2);
+	public List<BamUser> getAllTrainers() {
+		return userService.findByRole(2);
 	}
 
 	@RequestMapping(value = "AllAssociates", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	public List<BamUser> getAllAssociates(){
-		return bamUserService.findByRole(1);
+	public List<BamUser> getAllAssociates() {
+		return userService.findByRole(1);
 	}
 
 	@RequestMapping(value = "InBatch", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public List<BamUser> getUsersInBatch(HttpServletRequest request) {
+
 		//Get the batch id from the request
 		int batchId = Integer.parseInt( request.getParameter(batchID) );
 		
 		//Retrieve and return users in a batch from the database
-		return bamUserService.findUsersInBatch(batchId);
+		return userService.findUsersInBatch(batchId);
 	}
 
 	@RequestMapping(value = "Drop", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
 	public List<BamUser> dropUserFromBatch(HttpServletRequest request) {
+
 		//Get the user id from the request
-
 		int userId = Integer.parseInt( request.getParameter(userID) );
-		BamUser user = bamUserService.findUserById( userId );
-
+		BamUser user = userService.findUserById( userId );
 		int batchId = user.getBatch().getId();
 
 		// Drop user from the batch
 		user.setBatch(null);
 		user.setRole(0);
-		bamUserService.addOrUpdateUser(user);
-		
-		//Return users from batch without the user
-		return bamUserService.findUsersInBatch(batchId);
+		userService.addOrUpdateUser(user);
+
+		// Return users from batch without the user
+		return userService.findUsersInBatch(batchId);
 	}
 
 	
 	@RequestMapping(value="Update", method=RequestMethod.POST, produces="application/json")
 	public void updateUser(@RequestBody BamUser currentUser) {
-		BamUser user = bamUserService.findUserByEmail(currentUser.getEmail());
+		BamUser user = userService.findUserByEmail(currentUser.getEmail());
 		currentUser.setPwd(user.getPwd());
-		bamUserService.addOrUpdateUser(currentUser);
+		userService.addOrUpdateUser(currentUser);
 	}
 	
 	@RequestMapping(value="Register", method=RequestMethod.POST, produces="application/json")
 	public void addUser(@RequestBody BamUser currentUser) throws Exception {
-		if(bamUserService.findUserByEmail(currentUser.getEmail())==null){
+		if(userService.findUserByEmail(currentUser.getEmail())==null){
 			currentUser.setRole(1);
-			bamUserService.addOrUpdateUser(currentUser);
+			userService.addOrUpdateUser(currentUser);
 		} else {
 			throw new IllegalArgumentException("Email exists in database");
 		}	
@@ -115,10 +112,10 @@ public class UserController {
 
 	@RequestMapping(value="Reset", method=RequestMethod.POST, produces="application/java")
 	public void resetPassword(@RequestBody BamUser userNewPass) throws Exception{
-	  BamUser currentUser = bamUserService.findUserByEmail(userNewPass.getEmail());
+		BamUser currentUser = userService.findUserByEmail(userNewPass.getEmail());
 		if (currentUser.getPwd().equals(userNewPass.getPwd())) {
 			currentUser.setPwd(userNewPass.getPwd2());
-			bamUserService.addOrUpdateUser(currentUser);
+			userService.addOrUpdateUser(currentUser);
 
 		}else{
 			throw new IllegalArgumentException("Wrong password, password not changed");
@@ -130,42 +127,41 @@ public class UserController {
 	public List<BamUser> removeUser(HttpServletRequest request) {
 
 		//Get the user id from the request
-
 		int userId = Integer.parseInt( request.getParameter(userID) );
-		BamUser user = bamUserService.findUserById( userId );
-
+		BamUser user = userService.findUserById( userId );
 		int batchId = user.getBatch().getId();
 
 		// Set the user as inactive
 		Batch b = null;
 		user.setBatch(b);
-		bamUserService.addOrUpdateUser(user);
-		
-		//Return users from batch without the user
-		return bamUserService.findUsersInBatch(batchId);
+		userService.addOrUpdateUser(user);
+
+		// Return users from batch without the user
+		return userService.findUsersInBatch(batchId);
 	}
 
 	@RequestMapping(value = "Add", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
 	public List<BamUser> addUserToBatch(HttpServletRequest request) {
+
 		//Get the user id from the request
 		int userId = Integer.parseInt( request.getParameter(userID) );
 		//Get the batch to add the user to from the request
 		int batchId = Integer.parseInt( request.getParameter(batchID) );
 		
-		BamUser user = bamUserService.findUserById( userId );
+		BamUser user = userService.findUserById( userId );
 		
 		user.setBatch(batchService.getBatchById(batchId));
-		
-		bamUserService.addOrUpdateUser(user);
-		
-		return bamUserService.findUsersNotInBatch();
+
+		userService.addOrUpdateUser(user);
+
+		return userService.findUsersNotInBatch();
 	}
 
 	@RequestMapping(value = "NotInABatch", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
 	public List<BamUser> getUsersNotInBatch(HttpServletRequest request) {
-		return bamUserService.findUsersNotInBatch();
+		return userService.findUsersNotInBatch();
 	}
 
 }
--- BAM/src/main/java/com/bam/service/UsersDetailsService.java
+++ BAM/src/main/java/com/bam/service/UsersDetailsService.java
@@ -15,26 +15,62 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
+import com.bam.bean.Batch;
 import com.bam.bean.BamUser;
+import com.bam.repository.BatchRepository;
 import com.bam.repository.BamUserRepository;
 
 @Service("userDetailsService")
-public class BamUserDetailsService implements UserDetailsService {
-
-
-	@Autowired
-	private BamUserService bamUserService;
+@Transactional
+public class UsersDetailsService implements UserDetailsService {
+	
 	
 	@Autowired
-	private BamUserRepository bamUserRepository;
+	BamUserRepository dao;
 
+	@Autowired
+	BatchRepository bdao;
+
+	public void addOrUpdateUser(BamUser user) {
+		dao.save(user);
+
+	}
+
+	public List<BamUser> findAllUsers() {
+		return dao.findAll();
+
+	}
+
+	public List<BamUser> findByRole(int role) {
+		return dao.findByRole(role);
+	}
+
+	public BamUser findUserById(int userId) {
+		return dao.findByUserId(userId);
+	}
+
+	public BamUser findUserByEmail(String email) {
+		return dao.findByEmail(email);
+	}
+
+	/**
+	 * Get batch object by the id
+	 * Return users in the batch
+	 * @param batchId
+	 * @return
+	 */
+	public List<BamUser> findUsersInBatch(int batchId) {
+		
+		Batch batch = bdao.findById(batchId);
+		
+		return dao.findByBatch(batch);
+	}
 
 	/**
 	 * Returns users in the batch with a null
 	 */
 	public List<BamUser> findUsersNotInBatch() {
-		List<BamUser> users = bamUserRepository.findByBatch(null);
+		List<BamUser> users = dao.findByBatch(null);
 		for (int i = 0; i < users.size(); i++) {
 			if (users.get(i).getRole() != 1) {
 				users.remove(i);
@@ -45,29 +81,28 @@ public class BamUserDetailsService implements UserDetailsService {
 	}
 	
 	public UserDetails loadUserByUsername(final String email) throws UsernameNotFoundException {
-
-		BamUser user = bamUserService.findUserByEmail(email);
-
+		BamUser user = dao.findByEmail(email);
 		System.out.println(email);
 
 		return buildUserForAuthentication(user, buildUserAuthority(user));
 	}
 
-
-	// Converts Users user to org.springframework.security.core.userdetails.User
+	/**
+	 * 
+	 * @param user
+	 * @param authorities
+	 * @return
+	 * Converts Users user to org.springframework.security.core.userdetails.User
+	 */
 	private User buildUserForAuthentication(BamUser user, List<GrantedAuthority> authorities) {
-
 		return new User(user.getEmail(), user.getPwd(), true, true, true, true, authorities);
 	}
 
-	private List<GrantedAuthority> buildUserAuthority(BamUser user) {
+	private List<GrantedAuthority> buildUserAuthority(BamUser u) {
 
 		Set<GrantedAuthority> setAuths = new HashSet<GrantedAuthority>();
 
-
-		// Build user's authorities
-		setAuths.add(new SimpleGrantedAuthority("ROLE_"+String.valueOf(user.getRole())));
-
+		setAuths.add(new SimpleGrantedAuthority("ROLE_" + String.valueOf(u.getRole())));
 
 		List<GrantedAuthority> Result = new ArrayList<GrantedAuthority>(setAuths);
 
