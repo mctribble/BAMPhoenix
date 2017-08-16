64509bbe5930efd5e1cdfb531794b957e5a18edd
Brian McKalip <brmckalip@gmail.com>
2017-08-16 11:05:01 -0400

    merged old branch changes manually

--- BAM/src/main/webapp/static/controller/curriculumController.js
+++ BAM/src/main/webapp/static/controller/curriculumController.js
@@ -5,7 +5,16 @@
 app.controller(
 	"curriculumController",
 	
-	function($rootScope, $scope, $http) {
+	function($scope, $http) {
+		$scope.sanitizeString = function(str){
+			//replace spaces with underscores
+			str = str.replace(' ', '_');
+			//remove all non alphanumeric characters
+			str = str.replace(/\W/g, '');
+			console.log("new string: " + str);
+			return str;
+		}
+		
 		//constant array defining valid days of the week 
 		$scope.weekdays = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ];
 		
@@ -45,65 +54,20 @@ app.controller(
 		}
 		
 		//this object holds all curricula, which is an array of curriculum. Each has a type (eg java) and an array of versions. This will eventually be loaded via HTTP Get
-		$scope.curricula = 
-			[
-				{
-					type: 'java',
-					versions:[
-						{
-							weeks: [
-								{
-									days : 
-										[
-											{subtopics : [ 'Java', 'Subtopic 2', 'subtopic 3' ]},
-											{subtopics : [ 'Subtopic 1', 'Subtopic 2' ]},
-											{subtopics : [ 'Subtopic 1', 'Subtopic 2', 'subtopic 3', 'Subtopic 4' ]},
-											{subtopics : []}, 
-											{subtopics : [ 'Subtopic 1' ]}
-										]
-								},
-							
-								{
-									days : 
-										[
-											{subtopics : [ 'SQL', 'Subtopic 2', 'subtopic 3' ]},
-											{subtopics : [ 'Subtopic 1', 'Subtopic 2' ]},
-											{subtopics : [ 'Subtopic 1', 'Subtopic 2', 'subtopic 3', 'Subtopic 4' ]},
-											{subtopics : []}, 
-											{subtopics : [ 'Subtopic 1' ]}
-										]
-								},
-								
-								{
-									days : 
-										[
-											{subtopics : [ '.net', 'Subtopic 2', 'subtopic 3' ]},
-											{subtopics : [ 'Subtopic 1', 'Subtopic 2' ]},
-											{subtopics : [ 'Subtopic 1', 'Subtopic 2', 'subtopic 3', 'Subtopic 4' ]},
-											{subtopics : []}, 
-											{subtopics : [ 'Subtopic 1' ]}
-										]
-								},
-								
-								{
-									days : 
-										[
-											{subtopics : [ 'Subtopic 1', 'Subtopic 2', 'subtopic 3' ]},
-											{subtopics : [ 'Subtopic 1', 'Subtopic 2' ]},
-											{subtopics : [ 'Subtopic 1', 'Subtopic 2', 'subtopic 3', 'Subtopic 4' ]},
-											{subtopics : []}, 
-											{subtopics : [ 'Subtopic 1' ]}
-										]
-								}
-							]
-						}
-						//END OF FIRST VERSION
-					] //END OF VERSIONS
-				}, //END OF CURRICULUM
-			] //END OF CURRICULA ARRAY
+		$scope.curricula = [];
 		
 		$scope.addWeek = function(){
-			$scope.displayedCurriculum.weeks.push({days:[]});
+			var week = {
+				days:[
+						{subtopics : []},
+						{subtopics : []},
+						{subtopics : []},
+						{subtopics : []}, 
+						{subtopics : []}
+					]
+				};
+			console.log($scope.displayedCurriculum);
+			$scope.displayedCurriculum.weeks.push(week);
 		}
 		
 		$scope.deleteWeek = function(index){
@@ -113,19 +77,149 @@ app.controller(
 		}
 		
 		//when an existing curriculum is selected, it will be loaded into the template
-		$scope.setTemplate = function(type, version){
-			$scope.template.meta.type = $scope.curricula[type].type;
-			$scope.template.meta.version = type;
-			$scope.template.weeks = $scope.curricula[type].versions[version].weeks;
+		$scope.setTemplate = function(curriculum){
+			//attempt to look for curr in curricula object before doing http req (caching)
+			console.log("in setTemplate - Curriculum: ")
+			console.log(curriculum);
+			
+			if(curriculum){
+				var newCurriculum;
+				//do request
+				
+				//add curriculum object to the version
+				$scope.template.meta = curriculum
+				$scope.template.weeks; //r
+				//add the curriculum to the curricula object
+			}else{
+				
+			}
+			
+//			if(type == -1){
+//				//default template
+//				console.log("blank template");
+//				$scope.template.meta.type =  null;
+//				$scope.template.meta.version = null;
+//				$scope.template.weeks = [];
+//
+//			}else{
+//				$scope.template.meta.type = $scope.curricula[type].type;
+//				$scope.template.meta.version = version;
+//				$scope.template.weeks = $scope.curricula[type].versions[version].weeks;	
+//			}
 		}
-		
+			
 		//create a new curriculum with the template, if the template is null, a new curriculum will be created
 		$scope.newCurriculum = function(){
-			$scope.displayedCurriculum.meta.type = $scope.template.meta.type;
-			$scope.displayedCurriculum.meta.version = $scope.template.meta.version;
-			$scope.displayedCurriculum.weeks = $scope.template.weeks;
+//			$scope.displayedCurriculum.meta.type = $scope.template.meta.type;
+//			$scope.displayedCurriculum.meta.version = $scope.template.meta.version;
+//			$scope.displayedCurriculum.weeks = $scope.template.weeks;
+
+			console.log("creating next version of " + $scope.template.meta.type);
 			
+			var curriculum = {
+				meta: {
+					type: $scope.template.meta.type,
+					version: '0'
+				},
+				weeks: $scope.template.weeks
+			}
 			
+			//loop through the curricula looking for the curriculum type, when found count number of versions and set this curr. object's version to it
+			for(item in $scope.curricula){
+				if($scope.curricula[item].type == $scope.template.meta.type){
+					curriculum.meta.version = $scope.curricula[item].versions.length;
+				}
+			}
+			
+			$scope.displayedCurriculum = curriculum;
+			console.log($scope.displayedCurriculum);
 		}
+		
+		$scope.delectItems = function(){
+			var activeItems = document.getElementsByClassName("active");
+			for (var i = 0; i < activeItems.length; i++) {
+				   activeItems[i].classList.remove('active');
+			}
+		}
+		
+		$scope.saveCurriculum = function(){
+			console.log("saving curriculum");
+			if($scope.displayedCurriculum.meta.type){
+				console.log("type: " + $scope.displayedCurriculum.meta.type);
+				for(item in $scope.curricula){
+					console.log("checking type: " + $scope.curricula[item]);
+					if($scope.curricula[item].type == $scope.displayedCurriculum.meta.type){
+						console.log("found match - adding curriculum")
+						$scope.curricula[item].versions.push({weeks: $scope.displayedCurriculum.weeks});
+					}
+				}
+			}else{
+				var curriculum = {
+					type:'',
+					versions: [
+					]
+				};
+				
+				curriculum.type = "foo";
+				curriculum.versions.push({weeks:$scope.displayedCurriculum.weeks});
+				$scope.curricula.push(curriculum);
+			}
+		}
+		
+		$scope.getCurricula = function(){
+			$http({
+				url: "rest/api/v1/Curriculum/All",
+				method: "GET",
+				
+			}).then(function(response){
+				var curricula = response.data;
+				//parse the response into the local (front end) json object format
+				for(i in curricula){
+					var curriculum = curricula[i];
+//					console.log(curriculum);
+					//determine if $scope.curricula has a type of curriculum.Name already. If so add it as an additional version of the type
+					var curriculumTypeExists = false;
+					for(j in $scope.curricula){
+						var localCurricula = $scope.curricula[j];
+						if(localCurricula.curriculum_Name == curriculum.curriculum_Name){
+							console.log(localCurricula.curriculum_Name + " == " +  curriculum.curriculum_Name);
+							console.log("adding version " + curriculum.curriculum_Version + " of " + localCurricula.curriculum_Name + "")
+							curriculumTypeExists = true;
+							curriculum.weeks = [
+								{
+									days : []
+								}
+							]; //add a temp empty weeks array
+							//insert the curriculum into the existing curr type as a version of that type (as specified by the received object) 
+							$scope.curricula[j].versions.splice(curriculum.curriculum_Version, 0, curriculum);
+							break;
+						}
+					}
+					
+					//if a curriculum of type curriculum.curriculum_Name does not exist, add it as a new base curriculum type
+					if(!curriculumTypeExists){
+						console.log("adding curr type:");
+						console.log(curriculum);
+						var newCurriculum = {
+								type: curriculum.curriculum_Name,
+								versions: []
+						};
+						console.log("Version: " + curriculum.curriculum_Version);
+						curriculum.weeks = [
+							{
+								days : []
+							}
+						]; //add a temp empty weeks array
+						newCurriculum.versions.splice(curriculum.curriculum_Version, 0, curriculum);
+						console.log("New curriculum: ");
+						console.log(newCurriculum);
+						$scope.curricula.push(newCurriculum);
+					}
+				}
+				
+			});
+		}
+		//call the getCurriclua() function when the page is loaded
+		$scope.getCurricula();
 	}
 );
