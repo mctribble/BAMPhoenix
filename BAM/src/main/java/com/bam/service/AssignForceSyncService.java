dfc32864ac63b2e13c50389cc40f1deafe421036
Gil Birdsong <gibbirdsong@gmail.com>
2017-08-18 13:53:34 -0400

    docs added, sysouts removed

--- BAM/src/main/java/com/bam/service/AssignForceSyncService.java
+++ BAM/src/main/java/com/bam/service/AssignForceSyncService.java
@@ -16,6 +16,15 @@ import com.bam.bean.BamUser;
 import com.bam.bean.Batch;
 import com.bam.bean.BatchType;
 
+
+/**
+ * @author DillonT, GilB
+ * assignForceSyncService pulls the most current information from AssignForce
+ * and updates the BAM Batch, Users, and Batch_Type tables with the corresponding values.
+ * It utilizes 3 Objects, AssignForceBatch, AssignForceCurriculum, and AssignForceTrainer.
+ * These objects are used to hold data from a ResponseEntity, and are later used to 
+ * set data in objects relevant to persistence in the BAM database.
+ */
 @Service("assignForceSyncService")
 @Transactional
 public class AssignForceSyncService {
@@ -28,13 +37,18 @@ public class AssignForceSyncService {
 	
 	static RestTemplate restTemplate = new RestTemplate();
 	static String baseUrl = "http://assignforce.revaturelabs.com/api/v2/";
+	// Makes a call to the AssignForce API and builds POJOs from the returned JSON String.
 	static ResponseEntity<List<AssignForceBatch>> batches = restTemplate.exchange(baseUrl + "batch/",HttpMethod.GET,null, new ParameterizedTypeReference<List<AssignForceBatch>>(){});
 	
 	public void assignForceSync() {
-	
+		
+		// Object to hold data from AssignForce
 		AssignForceBatch batch = new AssignForceBatch();
+		// Object which will be persisted; populated with AssignForceBatch data.
 		Batch curr_batch = new Batch();
+		// Object necessary for creation of batch; populated with AssignForceBatch data.
 		BatchType type = new BatchType();
+		// Object necessary for creation of batch; populated with AssignForceBatch data.
 		BamUser bammy = new BamUser();
 		
 		// Cycling through all batches received from AssignForce.
@@ -43,33 +57,40 @@ public class AssignForceSyncService {
 			// Current cycle's batch defined from batches Response Entity.
 			batch = (AssignForceBatch) batches.getBody().get(i);
 			
+			// Filters our batches without trainers assigned.
 			if(batch.getTrainer() != null) {
 				
+				// Null check for curricula, assignment of curriculum values to batch_type.
 				if(batches.getBody().get(i).getCurriculum() != null) {
 					type.setId(batch.getCurriculum().getCurrId());
 					type.setLength(10);
 					type.setName(batches.getBody().get(i).getCurriculum().getName());
 				}
+				// Batch populated with data from AssignForceBatch
 				curr_batch.setName(batch.getName());
 				curr_batch.setStartDate(batch.getStartDate());
 				curr_batch.setEndDate(batch.getEndDate());
 				curr_batch.setId(batch.getID());
 				curr_batch.setType(type);
 				
+				// First and last names pulled from AssignForce trainer to search for current trainers.
 				String first_name = batch.getTrainer().getFirstName();
 				String last_name = batch.getTrainer().getLastName();
 				
+				// List comprised of trainers both in AssignForce and BAM
 				List<BamUser> BAMtrainers = uservice.getByFNameAndLName(first_name, last_name);
 	
-				
+				// If List is not empty and user is a trainer, trainer is assigned to batch.
 				if(!BAMtrainers.isEmpty() && BAMtrainers.get(0).getRole() == 2) {
 					bammy = BAMtrainers.get(0);
 					bammy.setAssignForce_ID(batch.getTrainer().getTrainerId());
 					curr_batch.setTrainer(bammy);
 				}else{
+					// If no trainer found, trainer set to null in batch.
 					curr_batch.setTrainer(null);
 				}
 				
+				// Batch is persisted, either added or updated.
 				bservice.addOrUpdateBatch(curr_batch);
 				
 			}
