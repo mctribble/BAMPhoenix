11fd88202ed05f3530dead68f7b6f667ecac906f
tomscheffer <tom.scheffer@yahoo.com>
2017-08-19 21:53:21 -0400

    Update navController.js

--- BAM/src/main/webapp/static/controller/navController.js
+++ BAM/src/main/webapp/static/controller/navController.js
@@ -6,12 +6,10 @@ app.controller('navController', function($rootScope, SessionService, $scope, $lo
 	$rootScope.userRole = SessionService.get("userRole");
 
 	$scope.$on('routeChangeStart', function(next, current) {
-		console.log('in the routeChangeStart');
 		SessionService.set("gotSubtopics", false);
 		var somePath = $location.path();
 
 		if (SessionService.get("currentUser").role < 2) {
-			console.log('if the curret user is less than 1' + SessionService.get("currentUser").role);
 			if (somePath == "/batchesAll" || somePath == "/batchesFuture"
 					|| somePath == "/batchesPast" || somePath == "/register"
 					|| somePath == "/associates" || somePath == "/editBatch") {
