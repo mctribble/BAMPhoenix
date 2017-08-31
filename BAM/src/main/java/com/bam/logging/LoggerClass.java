fae5d3d9a6bf170b237bbd616b4c738767f52c3d
Troy_K <textak64@gmail.com>
2017-08-31 10:39:42 -0400

    experimenting with splunkcimeventcollector object

--- BAM/src/main/java/com/bam/logging/LoggerClass.java
+++ BAM/src/main/java/com/bam/logging/LoggerClass.java
@@ -11,13 +11,18 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import com.splunk.logging.SplunkCimLogEvent;
+
 @Aspect
 @Component
 public class LoggerClass {
 
-  // Created Logger for Intercepting Methods and logging that Information 
+  // Created Logger for Intercepting Methods and logging that Information
 
   private static final Logger logger = LogManager.getRootLogger();
+  private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+  private static Long ServiceCalls = 0L;
+  private static Long ControllerCalls = 0L;
 
   /**
    * Logging Methods from the Batch service class
@@ -32,18 +37,18 @@ public class LoggerClass {
   public Object interceptService(ProceedingJoinPoint pjp) throws Exception {
     // return to always return join point objects so they are not consumed
     Object proceedObj = null;
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
-    String baseString = "(" + simpleDateFormat.format(new Date(System.currentTimeMillis())) + ")"
-                        + ": " + pjp.getSignature().getDeclaringTypeName()
-                        + " -> " + pjp.getSignature().getName()
-                        + " " + Arrays.toString(pjp.getArgs())
-                        + " => ";
+    SplunkCimLogEvent event = new SplunkCimLogEvent("service", Long.toString(++ServiceCalls));
+    event.addField("time", simpleDateFormat.format(new Date(System.currentTimeMillis())));
+    event.addField("signature", pjp.getSignature().getDeclaringTypeName() + " -> " + pjp.getSignature().getName());
+    event.addField("args", Arrays.toString(pjp.getArgs()));
 
     try {
       proceedObj = pjp.proceed();
-      logger.info(baseString + proceedObj);
+      event.addField("return", proceedObj);
+      logger.info("{" + event + "}");
     } catch (Throwable e) {
-      logger.error(baseString + e);
+      event.addThrowableWithStacktrace(e);
+      logger.error("{" + event + "}");
     }
 
     return proceedObj;
@@ -52,18 +57,18 @@ public class LoggerClass {
   @Around("execution(* com.bam.controller.*.*(..))")
   public Object interceptController(ProceedingJoinPoint pjp) throws Exception {
     Object proceedObj = null;
-    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
-    String baseString = "(" + simpleDateFormat.format(new Date(System.currentTimeMillis())) + ")"
-                        + ": " + pjp.getSignature().getDeclaringTypeName()
-                        + " -> " + pjp.getSignature().getName()
-                        + " " + Arrays.toString(pjp.getArgs())
-                        + " => ";
+    SplunkCimLogEvent event = new SplunkCimLogEvent("controller",  Long.toString(++ControllerCalls));
+    event.addField("time", simpleDateFormat.format(new Date(System.currentTimeMillis())));
+    event.addField("signature", pjp.getSignature().getDeclaringTypeName() + " -> " + pjp.getSignature().getName());
+    event.addField("args", Arrays.toString(pjp.getArgs()));
 
     try {
       proceedObj = pjp.proceed();
-      logger.info(baseString + proceedObj);
+      event.addField("return", proceedObj);
+      logger.info("{" + event + "}");
     } catch (Throwable e) {
-      logger.error(baseString + e);
+      event.addThrowableWithStacktrace(e);
+      logger.error("{" + event + "}");
     }
 
     return proceedObj;
