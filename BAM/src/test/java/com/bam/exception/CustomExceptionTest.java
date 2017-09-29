05a8f34d6d08b7ca38110ada706d46deca5c653a
Christopher Kaleb Hake <techgeek1000@gmail.com>
2017-09-29 16:10:00 -0400

    tests for CustomException

---
+++ BAM/src/test/java/com/bam/exception/CustomExceptionTest.java
@@ -0,0 +1,45 @@
+package com.bam.exception;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.Test;
+
+import javax.mail.MessagingException;
+
+import static org.junit.Assert.*;
+
+
+public class CustomExceptionTest
+{
+    @Test
+    public void exceptionsShouldHaveMessages() throws Exception
+    {
+        final String expectedMessage = "exception message";
+
+        try
+        {
+            throw new CustomException(new Exception(expectedMessage));
+        }
+        catch (CustomException e)
+        {
+            assert(e.getMessage().contains(expectedMessage));
+        }
+
+        try
+        {
+            throw new CustomException(new MessagingException(expectedMessage));
+        }
+        catch (CustomException e)
+        {
+            assert(e.getMessage().contains(expectedMessage));
+        }
+
+        try
+        {
+            throw new CustomException(expectedMessage);
+        }
+        catch (CustomException e)
+        {
+            assert(e.getMessage().contains(expectedMessage));
+        }
+    }
+}
