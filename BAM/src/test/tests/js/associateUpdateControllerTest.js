73b638a4cd00c0bf7b0b96f32be7c8bef0d825bf
Theara Ya <theara_ya@my.uri.edu>
2017-10-04 14:02:35 -0400

    associateControllerTest.js

--- BAM/src/test/tests/js/associateUpdateControllerTest.js
+++ BAM/src/test/tests/js/associateUpdateControllerTest.js
@@ -1,19 +1,50 @@
+beforeEach(module('bam'));
 describe('associateUpdateControllerTest', function() {
-    beforeEach(module('bam'));
-
     var $controller;
+
     beforeEach(inject(function(_$controller_) {
         $controller = _$controller_;
     }));
 
-    describe('test some function', function() {
-        it('this should produce this', function() {
+    describe('updateAssociate', function() {
+        it('updateAssociate responding to 200', inject(function($httpBackend) {
             var $scope = {};
             var controller = $controller('associateUpdateController', {$scope:$scope});
-            //now we can call the controller's scopes
-//            $scope.someFunction();
-            //perform the assertion test
-//            expect("result").toBe("expected");
-        });
+
+            $scope.updateAssociate();
+            $httpBackend
+                .when('POST', 'rest/api/v1/Users/Update')
+                .respond(200);
+            $httpBackend.flush();
+            expect($scope.updateDisplay).toBe(true);
+            expect($scope.updateMsg).toBe('Update Successful');
+            expect($scope.alertClass).toBe('alert alert-success');
+        }));
+        it('updateAssociate responding to 400', inject(function($httpBackend) {
+            var $scope = {};
+            var controller = $controller('associateUpdateController', {$scope:$scope});
+
+            $scope.updateAssociate();
+            $httpBackend
+                .when('POST', 'rest/api/v1/Users/Update')
+                .respond(400);
+            $httpBackend.flush();
+            expect($scope.updateDisplay).toBe(true);
+            expect($scope.updateMsg).toBe('Update Failed');
+            expect($scope.alertClass).toBe('alert alert-danger');
+        }));
+        it('updateAssociate responding to 500', inject(function($httpBackend) {
+             var $scope = {};
+             var controller = $controller('associateUpdateController', {$scope:$scope});
+
+             $scope.updateAssociate();
+             $httpBackend
+                .when('POST', 'rest/api/v1/Users/Update')
+                .respond(500);
+             $httpBackend.flush();
+             expect($scope.updateDisplay).toBe(true);
+             expect($scope.updateMsg).toBe('Update Failed');
+             expect($scope.alertClass).toBe('alert alert-danger');
+        }));
     });
-})
+});
