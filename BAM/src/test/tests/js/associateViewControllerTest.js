e978da9b1b5f414a70f5d6dd4860fb4504d114b3
Theara Ya <theara_ya@my.uri.edu>
2017-10-04 15:06:12 -0400

    associateViewController test added but empty

---
+++ BAM/src/test/tests/js/associateViewControllerTest.js
@@ -0,0 +1,25 @@
+//beforeEach(module('bam'));
+     //describe('associateViewControllerTest', function() {
+     //    var scope;
+     //    var SessionServiceMock;
+     //    var Controller;
+     //
+     //    beforeEach(function() {
+     //        SessionServiceMock = {
+     //            name : 'theara'
+     //        };
+     //    });
+     //    beforeEach(inject(function($rootScope, $controller) {
+     //        scope = $rootScope.$new();
+     //        Controller = $controller('associateViewController', {
+     //            $scope : scope,
+     //            DataService : SessionServiceMock
+     //        });
+     //    }))
+     //
+     //    describe('Retrieving associates for a particular batch given a batch id.', function() {
+     //        it('response status 200', inject(function($httpBackend) {
+     //            spyOn(SessionServiceMock, 'something').and.callThrough();
+     //        }));
+     //    });
+     //});
