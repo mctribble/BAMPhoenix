f9afa7411dbd765f1191db174988c6bd8aac3852
Theara Ya <theara_ya@my.uri.edu>
2017-10-05 09:27:12 -0400

    syncing

--- BAM/src/test/tests/js/associateViewControllerTest.js
+++ BAM/src/test/tests/js/associateViewControllerTest.js
@@ -1,25 +1,22 @@
-//beforeEach(module('bam'));
-     //describe('associateViewControllerTest', function() {
-     //    var scope;
-     //    var SessionServiceMock;
-     //    var Controller;
-     //
-     //    beforeEach(function() {
-     //        SessionServiceMock = {
-     //            name : 'theara'
-     //        };
-     //    });
-     //    beforeEach(inject(function($rootScope, $controller) {
-     //        scope = $rootScope.$new();
-     //        Controller = $controller('associateViewController', {
-     //            $scope : scope,
-     //            DataService : SessionServiceMock
-     //        });
-     //    }))
-     //
-     //    describe('Retrieving associates for a particular batch given a batch id.', function() {
-     //        it('response status 200', inject(function($httpBackend) {
-     //            spyOn(SessionServiceMock, 'something').and.callThrough();
-     //        }));
-     //    });
-     //});
+beforeEach(module('bam'));
+beforeEach(angular.mock.module({
+    'SessionService' : {
+        set: function() {
+        },
+        get : function() {
+            return null;
+        }
+    }
+}));
+describe('associateViewControllerTest', function() {
+    var $controller;
+    beforeEach(inject(function($_controller_) {
+        $controller = $_controller_;
+    }));
+    describe('testing the view', function() {
+        it('name should be loaded', function() {
+            var $scope = {};
+            var controller = $controller('associateViewController', {$scope:$scope});
+        })
+    });
+});
