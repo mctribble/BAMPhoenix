2c89e831b70043c9505256a493581f882bd93340
isaacshanholtz <isaacshanholtz@gmail.com>
2017-10-05 09:54:32 -0400

    Removed extra test code.

--- BAM/src/main/java/com/bam/Main.java
+++ BAM/src/main/java/com/bam/Main.java
@@ -1,9 +1,5 @@
 package com.bam;
 
-import com.bam.bean.BamUser;
-import com.bam.service.BamUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
@@ -12,22 +8,8 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 @EnableOAuth2Sso
 public class Main {
 
-    @Autowired
-    BamUserService bus;
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-    public CommandLineRunner run() {
-        return args -> {
-            try {
-                BamUser u = bus.findUserById(1);
-                System.out.println(u.getEmail());
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-        };
-    }
+    
 }
