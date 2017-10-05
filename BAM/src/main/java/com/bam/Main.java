244f79df53232fc7ae1345300f62c6afab420b5f
isaacshanholtz <isaacshanholtz@gmail.com>
2017-10-05 10:07:46 -0400

    Removed unnecessary extra test code from Main.java.

--- BAM/src/main/java/com/bam/Main.java
+++ BAM/src/main/java/com/bam/Main.java
@@ -1,31 +1,13 @@
 package com.bam;
 
-import com.bam.bean.BamUser;
-import com.bam.service.BamUserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
 
-    @Autowired
-    BamUserService bus;
-
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    public CommandLineRunner run() {
-        return args -> {
-            try {
-                BamUser u = bus.findUserById(1);
-                System.out.println(u.getEmail());
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-        };
-    }
 }
