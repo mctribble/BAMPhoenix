9729ec3c012072a06f15a85123dd1f67a8c59a6b
isaacshanholtz <isaacshanholtz@gmail.com>
2017-10-05 09:52:23 -0400

    Updated application.yml and Main.java for oauth2.

--- BAM/src/main/java/com/bam/Main.java
+++ BAM/src/main/java/com/bam/Main.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class Main {
 
     @Autowired
