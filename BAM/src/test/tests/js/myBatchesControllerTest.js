d748e27d368159b9d57d8174e06bd347f1c6397d
Theara Ya <theara_ya@my.uri.edu>
2017-10-09 11:28:52 -0400

    sync

--- BAM/src/test/tests/js/myBatchesControllerTest.js
+++ BAM/src/test/tests/js/myBatchesControllerTest.js
@@ -4,13 +4,24 @@ describe('myBatchesControllerTest', function() {
     var mockSession = {
         currentUser: {
             email: 'theara_ya@my.uri.edu'
+        },
+        currentBatch: {
+            batch : {
+                id : '13'
+            }
         }
     };
 
     beforeEach(angular.mock.module({
         'SessionService' : {
+            set: function(key, value) {
+                mockSession[key] = value;
+            },
             get : function(key) {
-                return eval("mockSession." + key);
+                return mockSession[key];
+            },
+            unset : function(key) {
+                delete mockSession[key];
             }
         }
     }));
@@ -22,17 +33,21 @@ describe('myBatchesControllerTest', function() {
     }));
 
     describe('getmyBatches', function() {
+        var $scope, controller;
+
+        beforeEach(function() {
+            $scope = {};
+            controller = $controller('myBatchesController', {$scope : $scope});
+        });
         it('getmyBatches responding to 200', inject(function($httpBackend) {
-            var $scope = {};
-            var controller = $controller('myBatchesController', {$scope:$scope});
 
             $scope.getmyBatches();
+
             $httpBackend
                 .when('GET', 'rest/api/v1/Batches/AllInProgress')
-                .respond(400);
-            $httpBackend.flush();
+                .respond(200, 'response');
+
             expect($scope.message).toBe(true);
-            expect($scope.msg).toBe('my current batches retrieved');
         }));
     });
 });
