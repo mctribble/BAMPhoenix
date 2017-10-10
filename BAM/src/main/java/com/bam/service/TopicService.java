6bf35e365f255ba875017c598fed2125277cd192
Christopher Hake <ckalebhake@gmail.com>
2017-10-10 14:00:42 -0400

    more fixes

--- BAM/src/main/java/com/bam/service/TopicService.java
+++ BAM/src/main/java/com/bam/service/TopicService.java
@@ -1,6 +1,7 @@
 package com.bam.service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,6 +33,13 @@ public class TopicService {
     b = batchRepistory.findById(batch);
     topicName = topicNameRepository.findById(topicNameId);
 
+    if (b == null)
+      throw new NoSuchElementException("that batch does not exist");
+    if (topicName == null)
+      throw new NoSuchElementException("that topic name does not exist");
+    if (weekNumber < 0)
+      throw new IllegalArgumentException("invalid week number");
+
     topic.setBatch(b);
     topic.setTopic(topicName);
     topic.setWeekNumber(weekNumber);
@@ -52,6 +60,8 @@ public class TopicService {
   }
 
   public void addOrUpdateTopicName(TopicName topic) {
+    if (topic == null)
+      throw new IllegalArgumentException("can't add or update null!");
     topicNameRepository.save(topic);
   }
 
